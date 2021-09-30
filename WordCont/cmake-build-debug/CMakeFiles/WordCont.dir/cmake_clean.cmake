2d8269ef0f40d084e1ea5ce45b3bdcd1e71c34e5
EgGsSon <89836972+EgGsSon@users.noreply.github.com>
2021-09-30 12:53:29 +0300

    Add files via upload

---
+++ WordCont/cmake-build-debug/CMakeFiles/WordCont.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WordCont.dir/main.c.o"
+  "WordCont"
+  "WordCont.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WordCont.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
